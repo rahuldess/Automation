a315fc4f0ea0946da62c26599439acfe53384529
Rahul Sambari <rsambari@paywith.com>
2020-09-21 22:16:15 -0700

    debugging issue

--- usa_visa_slots_checker.rb
+++ usa_visa_slots_checker.rb
@@ -20,9 +20,9 @@ begin
   browser.input(name: 'commit').click
 
   # Click on Continue button
-  #browser.link(href: '/en-ca/niv/schedule/31865037/continue_actions').click
+  browser.link(href: '/en-ca/niv/schedule/31865037/continue_actions').when_present.click
   #browser.link(xpath: '//*[@id="main"]/div[2]/div[3]/div[1]/div/div/div[1]/div[2]/ul/li/a').click
-  browser.li(xpath: '//*[@id="main"]/div[2]/div[3]/div[1]/div/div/div[1]/div[2]/ul/li').click
+  #browser.li(xpath: '//*[@id="main"]/div[2]/div[3]/div[1]/div/div/div[1]/div[2]/ul/li').click
 
   # Click on 'Reschedule Appointment' 
   browser.execute_script('$("#forms > ul > li:nth-child(4) > div").css("display", "block")')
