9977cd436bdbce13758bf5bd99377bac39de77c5
Rahul Sambari <rsambari@paywith.com>
2020-09-30 17:24:30 -0700

    slot booking automated

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source "https://rubygems.org"
 
-ruby "2.6.6"
+ruby "2.6.5"
 
 gem "watir"
 gem 'twilio-ruby'
+gem 'byebug'
+gem 'nokogiri'
 
--- usa_visa_slots_checker.rb
+++ usa_visa_slots_checker.rb
@@ -1,5 +1,7 @@
 require 'watir'
 require 'twilio-ruby'
+require 'byebug'
+require 'nokogiri'
 
 begin
   # Setting chrome for heroku
@@ -20,28 +22,54 @@ begin
   browser.input(name: 'commit').click
 
   # Click on Continue button
-  browser.link(href: '/en-ca/niv/schedule/31865037/continue_actions').when_present.click
-  #browser.link(xpath: '//*[@id="main"]/div[2]/div[3]/div[1]/div/div/div[1]/div[2]/ul/li/a').click
-  #browser.li(xpath: '//*[@id="main"]/div[2]/div[3]/div[1]/div/div/div[1]/div[2]/ul/li').click
+  browser.link(href: "/en-ca/niv/schedule/#{ENV['ACCOUNT_NO']}/continue_actions").click
 
   # Click on 'Reschedule Appointment' 
   browser.execute_script('$("#forms > ul > li:nth-child(4) > div").css("display", "block")')
-  browser.link(href: '/en-ca/niv/schedule/31865037/appointment').click
+  browser.link(href: "/en-ca/niv/schedule/#{ENV['ACCOUNT_NO']}/appointment").click
 
-  # Click on 'Date of Appointment field' 
-  browser.input(id: "appointments_consulate_appointment_date").click
+  # Clicking the location dropdown
+  # NOTES: Switching between different cities just to trigger ajax call to fetch slots
+  browser.select_list(id: "appointments_consulate_appointment_facility_id").option(text: 'Vancouver').select
+  browser.select_list(id: "appointments_consulate_appointment_facility_id").option(text: 'Calgary').select
+  browser.select_list(id: "appointments_consulate_appointment_facility_id").option(text: 'Vancouver').select
 
-  # Each time checks 2 months 
-  ENV['NO_OF_TIMES'].to_i.times do
-    available_slots = browser.execute_script('return $("table.ui-datepicker-calendar > tbody > tr > :not(td.ui-datepicker-unselectable)").length')
+  # Click on 'Date of Appointment field'
+  browser.input(id: "appointments_consulate_appointment_date").when_present.click
 
-    if available_slots > 0
-      twilio_client.messages.create(from: ENV['FROM_PHONE'], to: ENV['TO_PHONE'], body: 'Slot Available. Check it quick')
+  10.times do
+    available_slots = browser.execute_script('return $("table.ui-datepicker-calendar > tbody > tr > :not(td.ui-datepicker-unselectable)")')
+
+    if available_slots.length > 0
+      first_slot      = available_slots.first
+      first_slot_html = first_slot.html 
+
+      slot = Nokogiri::HTML.parse(first_slot_html).root.children.children
+
+      first_available_month = slot.attr('data-month').value.to_i
+      first_available_year  = slot.attr('data-year').value.to_i
+      first_available_date  = slot.children.children.text.to_i
+
+      if first_available_year > ENV['CURRENT_SLOT_YEAR'].to_i && 
+          first_available_month > ENV['CURRENT_SLOT_MONTH'].to_i && 
+          first_available_date > ENV['CURRENT_SLOT_DATE'].to_i
+
+        return
+      end
+      
+      # Select the first slot available
+      # Select the first time slot available
+      # Confirm re-schedule 
+      first_slot.click
+      browser.select_list(id: "appointments_consulate_appointment_time").option(index: 1).select
+      browser.input(id: "appointments_submit").click
+
+      twilio_client.messages.create(from: ENV['FROM_PHONE'], to: ENV['TO_PHONE'], body: "Slot Booked. Please check!")
       return
+    else
+      # Click on next button to check for next months
+      browser.link(xpath: '//*[@id="ui-datepicker-div"]/div[2]/div/a').click
     end
-
-    # Click on next button to check for next months
-    browser.link(xpath: '//*[@id="ui-datepicker-div"]/div[2]/div/a').click
   end
 
   browser.close
@@ -49,6 +77,5 @@ rescue Exception => error
   puts "--------ERROR-------"
   puts error
   puts "--------------------"
-
   #twilio_client.messages.create(from: ENV['FROM_PHONE'], to: ENV['DEBUG_TO_PHONE'], body: 'USA Slot check ERROR!')
 end
