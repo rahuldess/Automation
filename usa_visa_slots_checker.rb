491f5b1c4d3dae89557e5490db9ca76c0d8999c0
Rahul Sambari <rsambari@paywith.com>
2020-09-30 22:24:40 -0700

    lil refactoring

--- usa_visa_slots_checker.rb
+++ usa_visa_slots_checker.rb
@@ -3,6 +3,20 @@ require 'twilio-ruby'
 require 'byebug'
 require 'nokogiri'
 
+twilio_account     = ENV['TWILIO_ACCT']
+twilio_token       = ENV['TWILIO_TOKEN']
+login_email        = ENV['LOGIN_EMAIL']
+login_pass         = ENV['LOGIN_PASSWORD']
+account_no         = ENV['ACCOUNT_NO']
+
+current_slot_year  = ENV['CURRENT_SLOT_YEAR'].to_i
+current_slot_date  = ENV['CURRENT_SLOT_DATE'].to_i
+current_slot_month = ENV['CURRENT_SLOT_MONTH'].to_i
+
+from_phone         = ENV['FROM_PHONE']
+to_phone           = ENV['TO_PHONE']
+debug_to_phone     = ENV['DEBUG_TO_PHONE']
+
 begin
   # Setting chrome for heroku
   Selenium::WebDriver::Chrome.path        = "/app/.apt/usr/bin/google-chrome"
@@ -10,23 +24,23 @@ begin
 
   # Select the browser
   browser       = Watir::Browser.new :chrome
-  twilio_client = Twilio::REST::Client.new ENV['TWILIO_ACCT'], ENV['TWILIO_TOKEN']
+  twilio_client = Twilio::REST::Client.new twilio_account, twilio_token 
 
   # Open the URL
   browser.goto "https://ais.usvisa-info.com/en-ca/niv/users/sign_in"
 
   # Fill the credentials
-  browser.text_field(id: 'user_email').set(ENV['LOGIN_EMAIL'])
-  browser.text_field(id: 'user_password').set(ENV['LOGIN_PASSWORD'])
+  browser.text_field(id: 'user_email').set(login_email)
+  browser.text_field(id: 'user_password').set(login_pass)
   browser.execute_script('$("div.icheckbox > input#policy_confirmed").click()')
   browser.input(name: 'commit').click
 
   # Click on Continue button
-  browser.link(href: "/en-ca/niv/schedule/#{ENV['ACCOUNT_NO']}/continue_actions").click
+  browser.link(href: "/en-ca/niv/schedule/#{account_no}/continue_actions").click
 
   # Click on 'Reschedule Appointment' 
   browser.execute_script('$("#forms > ul > li:nth-child(4) > div").css("display", "block")')
-  browser.link(href: "/en-ca/niv/schedule/#{ENV['ACCOUNT_NO']}/appointment").click
+  browser.link(href: "/en-ca/niv/schedule/#{account_no}/appointment").click
 
   # Clicking the location dropdown
   # NOTES: Switching between different cities just to trigger ajax call to fetch slots
@@ -50,13 +64,8 @@ begin
       first_available_year  = slot.attr('data-year').value.to_i
       first_available_date  = slot.children.children.text.to_i
 
-      if first_available_year > ENV['CURRENT_SLOT_YEAR'].to_i && 
-          first_available_month > ENV['CURRENT_SLOT_MONTH'].to_i && 
-          first_available_date > ENV['CURRENT_SLOT_DATE'].to_i
+      return if first_available_year > current_slot_year && first_available_month > current_slot_month && first_available_date > current_slot_date
 
-        return
-      end
-      
       # Select the first slot available
       # Select the first time slot available
       # Confirm re-schedule 
@@ -64,7 +73,8 @@ begin
       browser.select_list(id: "appointments_consulate_appointment_time").option(index: 1).select
       browser.input(id: "appointments_submit").click
 
-      twilio_client.messages.create(from: ENV['FROM_PHONE'], to: ENV['TO_PHONE'], body: "Slot Booked. Please check!")
+      # Send message to notify the user
+      twilio_client.messages.create(from: from_phone, to: to_phone, body: "Slot Booked. Please check!")
       return
     else
       # Click on next button to check for next months
@@ -78,5 +88,5 @@ rescue Exception => error
   puts error
   puts "--------------------"
 
-  twilio_client.messages.create(from: ENV['FROM_PHONE'], to: ENV['DEBUG_TO_PHONE'], body: 'USA Slot check ERROR!')
+  twilio_client.messages.create(from: from_phone, to: debug_to_phone, body: 'USA Slot check ERROR!')
 end
